b98e415cabd9c2313ab9993729f22d4f0be1090e
Joel Brock <joelbrock@gmail.com>
2013-08-17 21:23:35 -0500

    typo

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
@@ -1,7 +1,7 @@
 <?php
 
 $my_menu = array(
-	"Salab Bar - $6.99/lb" => "9010",
+	"Salad Bar - $6.99/lb" => "9010",
 	"8 oz Soup - $2.99" => "9003",
 	"16 oz Soup - $4.99" => "9004",
 	"Soup Cup Single - $0.30" => "9091"
