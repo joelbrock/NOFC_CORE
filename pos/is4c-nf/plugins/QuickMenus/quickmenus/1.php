573518e75af367dee2993a65f375f7f3174adac5
Joel Brock <joelbrock@gmail.com>
2013-08-19 13:25:59 -0500

    Weeee….quick menus!

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
@@ -4,6 +4,7 @@ $my_menu = array(
 	"Salad Bar - $6.99/lb" => "9010",
 	"8 oz Soup - $2.99" => "9003",
 	"16 oz Soup - $4.99" => "9004",
+	"Sushi - ENTER PRICE" => "9190",
 	"Soup Cup Single - $0.30" => "9091"
 );
 
